8e613cb07d43ede33f8759f4729578a8c6f74003
hau <hau@cafe24corp.vn>
2023-05-18 16:35:57 +0700

    archive product

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -9,15 +9,15 @@
 */
 
 function fancy_lab_scripts() {
-    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
-    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.3.1', 'all' );
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '5.3.0', true );
+    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '5.3.0', 'all' );
     wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
     //sử dụng filemtime để bỏ qua cache
 }
 //wp_enqueue_scripts để thêm js và css vào site
 add_action( 'wp_enqueue_scripts', 'fancy_lab_scripts' );
 
-function fancy_lab_cofig() {
+function fancy_lab_config() {
     //tạo mới menu trong dashboard
     register_nav_menus(
         array(
@@ -25,6 +25,31 @@ function fancy_lab_cofig() {
             'fancy_lab_footer_menu' => 'Fancy Lab Footer Menu'
         )
     );
+
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 500,
+        'single_image_width' => 500,
+        'product_grid' => array(
+            'default_rows' => 10,
+            'min_rows' => 5,
+            'max_rows' => 10,
+            'default_columns' => 1,
+            'min_columns' => 1,
+            'max_columns' => 1,
+        )
+    ));
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+
+    if ( ! isset( $content_width ) ) {
+        $content_width = 600;
+    }
+
 }
 //after_setup_theme hiển thị những elements sau khi được tùy chỉnh và lưu trong admin
-add_action( 'after_setup_theme', 'fancy_lab_cofig', 0 );
+add_action( 'after_setup_theme', 'fancy_lab_config', 0 );
+
+if (class_exists('Woocommerce')) { //check condition when plugin Woocommerce not extis
+    require get_template_directory() . '/inc/wc-modification.php';
+}
